887457498303785729ed4aee6ec3888dc0bbf75a
duanhouhai <duanhouhai@shinezone.com>
2018-01-11 14:41:29 +0800

    修正错误。

--- Editor/AssetBundleDataSource/AssetDatabaseJsonDataSource.cs
+++ Editor/AssetBundleDataSource/AssetDatabaseJsonDataSource.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEditor;
+using UnityEngine;
+using System.Linq;
 
 namespace AssetBundleBrowser.AssetBundleDataSource
 {
--- Editor/AssetBundleDataSource/DatabaseUtil.cs
+++ Editor/AssetBundleDataSource/DatabaseUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using YH.AssetManager;
+using UnityEngine;
 
 namespace AssetBundleBrowser.AssetBundleDataSource
 {
